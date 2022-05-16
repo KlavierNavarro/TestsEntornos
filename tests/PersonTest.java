01422eade349e5ce27cf6580da758dcefdabf9d7
Klavier <klaviernavarro@gmail.com>
2022-05-16 16:12:54 +0200

    si

--- tests/PersonTest.java
+++ tests/PersonTest.java
@@ -5,15 +5,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
+    Person p;
 
     @BeforeEach
     void setUp() {
-        Person p = new Person();
+        p = new Person();
     }
 
     @Test
     void setEmail() {
+        p.setEmail("holadiego");
+        assertEquals("", p.getEmail());
 
+        p.setEmail("afhshyfd@hotmail.com");
+        assertEquals("afhshyfd@hotmail.com", p.getEmail());
+
+        p.setEmail("");
+        assertEquals("", p.getEmail());
+
+        p.setEmail(null);
+        assertEquals("", p.getEmail());
+
+        p.setEmail("..@.com");
+        assertEquals("", p.getEmail());
     }
 
     @Test
